02c573c2eb9854b6021735f41e7dfa1bf5ca5065
farendivaz <farendivaz@gmail.com>
2022-05-28 07:27:45 +0700

    testing

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>This is Contact</div>;
+  return (
+    <div>
+      <h1>HELLO FROM VERCEL</h1>
+      <h4>THIS IS CONTACT FORM</h4>
+    </div>
+  );
 };
 
 export default Contact;
