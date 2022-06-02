ca708b5a8c1edc69f0fd348aa659f48499c7cdb4
farendivaz <farendivaz@gmail.com>
2022-06-03 02:55:24 +0700

    add responsive to the Error Page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -6,7 +6,7 @@ import ErrorIcon from "../images/svg/error_icon.svg";
 const ErrorPage = () => {
   return (
     <ErrorWrapper>
-      <img src={ErrorIcon} alt="Error Page" />
+      <img src={ErrorIcon} alt="Error 404 Icon" />
       <h1>Page Not Found</h1>
       <p>Sorry, We couldn't find the page you're looking for</p>
       <Button>
@@ -17,14 +17,14 @@ const ErrorPage = () => {
 };
 
 const ErrorWrapper = styled.main`
+  height: 380px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 3rem auto 5rem auto;
+  margin: 5rem auto 5rem auto;
   img {
-    width: 650px;
-    height: 350px;
+    width: 500px;
   }
   h1 {
     margin: 1rem 0 0 0;
@@ -33,6 +33,25 @@ const ErrorWrapper = styled.main`
     width: 40%;
     text-align: center;
     margin: 0 0 1rem 0;
+  }
+  @media screen and (max-width: 960px) {
+    height: 440px;
+    margin: 5rem auto 30rem auto;
+    img {
+      width: 500px;
+    }
+    h1 {
+      margin: 0;
+    }
+  }
+  @media screen and (max-width: 670px) {
+    margin: 1rem auto 10rem auto;
+    img {
+      width: 400px;
+    }
+    h1 {
+      margin: 0;
+    }
   }
 `;
 
