307350548db46a27ac04f1ca269882ef61659128
farendivaz <farendivaz@gmail.com>
2022-07-27 21:34:06 +0700

    add description to the data

--- src/data/data.js
+++ src/data/data.js
@@ -6,9 +6,9 @@ export const projects = [
     title: "Personal Website",
     imageSrc: Image,
     imageAlt: "Personal Website that contains all movie from rating etc..",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est provident ratione incidunt explicabo quo obcaecati consequuntur totam iste quidem ea omnis, voluptatem similique, eveniet voluptas necessitatibus minus tempore illum beatae.",
-    demo: "https://google.com",
-    github: "https://github.com",
+    desc: "Personal webiste that contains my portfolio and contact. I built with React, React router and styling with styled-components",
+    demo: "https://www.farendivaz.live",
+    github: "https://github.com/farendivaz/farendivaz.live",
   },
   {
     id: 2,
