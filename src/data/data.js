d499bafdbffc1689d3b9d009729b60ef97191161
farendivaz <farendivaz@gmail.com>
2022-07-27 21:39:02 +0700

    change description

--- src/data/data.js
+++ src/data/data.js
@@ -6,9 +6,10 @@ export const projects = [
     title: "Personal Website",
     imageSrc: Image,
     imageAlt: "Personal Website that contains all movie from rating etc..",
-    desc: "Personal webiste that contains my portfolio and contact. I built with React, React router and styling with styled-components",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est provident ratione incidunt explicabo quo obcaecati consequuntur totam iste quidem ea omnis, voluptatem similique, eveniet voluptas necessitatibus minus tempore illum beatae.",
     demo: "https://www.farendivaz.live",
     github: "https://github.com/farendivaz/farendivaz.live",
+    // Personal webiste that contains my portfolio and contact. I built with React, React router and styling with styled-components
   },
   {
     id: 2,
