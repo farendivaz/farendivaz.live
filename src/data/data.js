aa4f67402cbb488cdd80fbf99a117be8cb5015c6
farendivaz <farendivaz@gmail.com>
2022-08-14 14:51:33 +0700

    add new project to the projects

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,13 @@
 import PersonalWebsiteImage from "../assets/images/farendivaz.png";
 import GithubFinderImage from "../assets/images/github-finder.png";
+import MovieSearchImage from "../assets/images/movie-search.png";
 
 export const projects = [
   {
     id: 1,
     title: "Personal Website",
     imageSrc: PersonalWebsiteImage,
-    imageAlt: "Farendivaz Personal Website preview",
+    imageAlt: "Farendivaz Personal Website preview image",
     desc: "This is my first Personal website, which contains my portfolio and contact. I built this website with React, React router, and the styling is done with styled-components. This website Deploy on Vercel. I would highly recommend Vercel, because is the easiest platform to deploy static website",
     demo: "https://www.farendivaz.live",
     github: "https://github.com/farendivaz/farendivaz.live",
@@ -15,19 +16,19 @@ export const projects = [
     id: 2,
     title: "Github Finder",
     imageSrc: GithubFinderImage,
-    imageAlt: "Github Finder website preview",
+    imageAlt: "Github Finder website preview image",
     desc: "I completed this project during React Front to Back 2022 by Brad Traversy on Udemy. I built this project with React & React Router and the styling is done with Tailwind and its framework DaisyUI. And connects with Github API to search for users and details about the selected user.",
     demo: "https://github-finder-farendivaz.vercel.app/",
     github: "https://github.com/farendivaz/github-finder",
   },
   {
     id: 3,
-    title: "Personal Website",
-    imageSrc: PersonalWebsiteImage,
-    imageAlt: "Personal Website that contains all movie from rating etc..",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est provident ratione incidunt explicabo quo obcaecati consequuntur totam iste quidem ea omnis, voluptatem similique, eveniet voluptas necessitatibus minus tempore illum beatae.",
-    demo: "https://google.com",
-    github: "https://github.com",
+    title: "Movie Search",
+    imageSrc: MovieSearchImage,
+    imageAlt: "Movie search app preview image",
+    desc: "I built this project with React and CSS. This project connects with OMDB API to search for movies and detail about the movie. This project is one of my first projects, also I learned how to fetch a third-party API in ReactJS and display the data in the Frontend",
+    demo: "https://movie-site-one.vercel.app/",
+    github: "https://github.com/farendivaz/movie-site-app",
   },
   {
     id: 4,
